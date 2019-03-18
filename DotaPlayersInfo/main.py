04f87949db4dc75396ca94c9467e10eba4dc133a
Herring0 <38766027+Herring0@users.noreply.github.com>
2019-03-18 22:46:06 +0300

    Add files via upload

--- DotaPlayersInfo/main.py
+++ DotaPlayersInfo/main.py
@@ -10,22 +10,29 @@ import serverlogparser
 path = 'C:/Program Files (x86)/Steam/steamapps/common/dota 2 beta/game/dota/server_log.txt'
 ids = serverlogparser.LogParser(path).getPlayerFromLastGame()
 
+
+
 print('starting...')
 for user_id in ids:
-	player = lastmatches.LastMatches(user_id)
-	mr, hn = player.getDetails()
-	print('profile name: {}'.format(player.getProfileName()))
-	for i in range(0, len(hn)):
-		if len(hn[i]) < 19:
-			while len(hn[i]) < 19:
-				hn[i] += " "
-		print('   ', hn[i], ' | ', mr[i])
-		time.sleep(.007)
-	print()
-	print('WINS: {0} | LOSSES: {1}'.format(player.getWins(), player.getLoses()), )
-	print('----------------------------------------------------------------------')
-	if not ids.index(user_id) == len(ids)-1:
+	try:
+		player = lastmatches.LastMatches(user_id)
+		mr, hn = player.getDetails()
+		print('profile name: {}'.format(player.getProfileName()))
+		for i in range(0, len(hn)):
+			if len(hn[i]) < 19:
+				while len(hn[i]) < 19:
+					hn[i] += " "
+			print('   ', hn[i], ' | ', mr[i])
+			time.sleep(.007)
+		print()
+		print('WINS: {0} | LOSSES: {1}'.format(player.getWins(), player.getLoses()), )
+		print('----------------------------------------------------------------------')
+		if not ids.index(user_id) == len(ids)-1:
+			print('processing...')
+		print()
+	except KeyError as e:
+		print('PROFILE IS HIDDEN')
+		print('----------------------------------------------------------------------')
 		print('processing...')
-	print()
 print('end')
 input()
